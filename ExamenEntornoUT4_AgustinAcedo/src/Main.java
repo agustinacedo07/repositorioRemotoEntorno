7a4889734022f618a4685b9697d05b0c4f4a9f3e
ponaquitunombre <ponaquitunombre@1DAM-O01>
2018-03-08 09:52:10 +0100

    Subida a repositorio local 1.0

---
+++ ExamenEntornoUT4_AgustinAcedo/src/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    	//Cambio el nombre de la variable miCuenta por cuenta1
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+            //modificado
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
